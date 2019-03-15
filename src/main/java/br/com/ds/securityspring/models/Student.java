d7276aa2862c46303c98410dd78d813e182b5300
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-15 13:54:47 -0300

    Anotations Id and Generated Value

--- src/main/java/br/com/ds/securityspring/models/Student.java
+++ src/main/java/br/com/ds/securityspring/models/Student.java
@@ -4,14 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Student {
 
     @Getter @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer studentId;
+
     @Getter @Setter
     private String name;
+    
     @Getter @Setter
     private Integer age;
 }
